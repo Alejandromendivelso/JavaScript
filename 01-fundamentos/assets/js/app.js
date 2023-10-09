985aa39aba6c80a58ffb02b0ad97818866d68d85
Alejandro Mendivelso Acosta <amendivels3@uniminuto.edu.co>
2023-10-09 16:23:09 -0500

    Curso JS
    primero parte

---
+++ 01-fundamentos/assets/js/app.js
@@ -0,0 +1,20 @@
+// console.log('hola Mundo');
+//var b = 10;
+// const c = 10;
+//c = 20; // esto no se puede hacer 
+// foma corta de la siguiente maner 
+let a = 10, 
+    b = 20, 
+    c ='hola ', 
+    d='mundo',  
+    x= a +b;
+
+const saludo = c + d ;
+//console.log('a',a);
+//console.log({b});
+//console.log(c);
+
+c = 'hola de nuevo';
+//console.table({a,b,c,d,x});
+
+var outerWidth = 100000000;
---
+++ 01-fundamentos/assets/js/primitivos.js
@@ -0,0 +1,14 @@
+
+let nomblre =  'peter ';
+console.log('nombre');
+
+nombre = 'Ben';
+console.log(nombre);
+
+nombre ="Tia";
+console.log(typeof nombre);
+
+nombre = 123;
+console.log(typeof nombre);
+
+let esM
